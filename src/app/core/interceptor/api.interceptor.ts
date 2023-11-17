697c1ea4c2bad8ca85682847c51569dc5f5b6c24
yohagos <yosef.hagos@googlemail.com>
2023-11-17 15:51:58 +0100

    feat: first implementations

---
+++ src/app/core/interceptor/api.interceptor.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Observable } from "rxjs";
+
+
+@Injectable({
+  providedIn: "root"
+})
+export class ApiInterceptor implements HttpInterceptor {
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    const apiReq = req.clone({
+      url: ``
+    })
+    return next.handle(apiReq);
+  }
+}
