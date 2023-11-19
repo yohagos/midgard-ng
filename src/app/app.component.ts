c3558bea61410ae6f955ab3cdf6e673c0763f93a
yohagos <62908401+yohagos@users.noreply.github.com>
2023-11-19 13:29:06 +0100

    8 material design modifications (#9)
    * feat: first implementations
    
    * fix: themes and toolbar
    
    * theme and menu implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Inject, Renderer2 } from '@angular/core';
+import { DOCUMENT } from "@angular/common";
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'midgard-ng';
+export class AppComponent implements OnInit {
+  title = 'Midgard'
+  menuList: string[] = ['Sign In', 'Log In', 'Impressum']
+
+  constructor(
+    private renderer: Renderer2,
+    @Inject(DOCUMENT) private document: Document
+  ) {
+
+  }
+
+  ngOnInit() {
+    this.renderer.addClass(this.document.body, 'lightTheme')
+  }
+
+  changeTheme() {
+    if(this.document.body.classList.contains('lightTheme')) {
+      this.renderer.removeClass(this.document.body, 'lightTheme');
+      this.renderer.addClass(this.document.body, 'darkTheme')
+    } else if (this.document.body.classList.contains('darkTheme')) {
+      this.renderer.removeClass(this.document.body, 'darkTheme')
+      this.renderer.addClass(this.document.body, 'lightTheme');
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonToggleModule } from "@angular/material/button-toggle";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatMenuModule } from "@angular/material/menu";
+import { MatSelectModule } from "@angular/material/select";
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
@@ -12,7 +23,18 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatMenuModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
