68299e1223759f8159f838a566efcf866f796812
yohagos <62908401+yohagos@users.noreply.github.com>
2023-11-26 13:29:09 +0100

    18 navigation (#19)
    * fix: update app modules
    
    * fix: clean up app component
    
    * feat: refactoring header component, creating nav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,25 +5,38 @@ import { ProfileComponent } from './features/profile/profile.component';
 import { LoginComponent } from './features/login/login.component';
 import { SignupComponent } from './features/signup/signup.component';
 import { AuthGuardService } from './core/services/auth-guard.service';
+import { NavigationComponent } from './shared/navigation/navigation.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: HomeComponent
+    component: NavigationComponent,
+    children: [
+      {
+        path: 'home',
+        component: HomeComponent
+      },
+      {
+        path: 'login',
+        component: LoginComponent
+      },
+      {
+        path: 'signup',
+        component: SignupComponent
+      },
+      {
+        path: 'profile',
+        component: ProfileComponent,
+        canActivate: [AuthGuardService]
+      },
+      {
+        path: '**',
+        redirectTo: '/home',
+        pathMatch: 'full'
+      }
+    ]
   },
-  {
-    path: 'login',
-    component: LoginComponent
-  },
-  {
-    path: 'signup',
-    component: SignupComponent
-  },
-  {
-    path: 'profile',
-    component: ProfileComponent,
-    canActivate: [AuthGuardService]
-  }
+
 ];
 
 @NgModule({
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,53 +1,10 @@
-import { Component, OnInit, Inject, Renderer2 } from '@angular/core';
-import { DOCUMENT } from "@angular/common";
-import { Router } from '@angular/router';
-
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  title = 'Midgard'
-  menuList: string[] = ['Home', 'Sign Up', 'Log In', 'Profile']
-
-  constructor(
-    private renderer: Renderer2,
-    @Inject(DOCUMENT) private document: Document,
-    private router: Router
-  ) {
-
-  }
-
-  ngOnInit() {
-    this.renderer.addClass(this.document.body, 'lightTheme')
-  }
-
-  changeTheme() {
-    if(this.document.body.classList.contains('lightTheme')) {
-      this.renderer.removeClass(this.document.body, 'lightTheme');
-      this.renderer.addClass(this.document.body, 'darkTheme')
-    } else if (this.document.body.classList.contains('darkTheme')) {
-      this.renderer.removeClass(this.document.body, 'darkTheme')
-      this.renderer.addClass(this.document.body, 'lightTheme');
-    }
-  }
-
-  changeTo(page: string) {
-    switch (page) {
-      case 'Home':
-        this.router.navigate(['/'])
-        break
-      case 'Log In':
-        this.router.navigate(['/login'])
-        break
-      case 'Sign Up':
-        this.router.navigate(['/signup'])
-        break
-      default:
-        console.log("No such page")
-        break
-    }
-  }
+export class AppComponent {
+  
 }
