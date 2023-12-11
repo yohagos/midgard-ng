4b15c6553dd4d41a10509e2d30cb62cff893672a
yohagos <yosef.hagos@googlemail.com>
2023-12-11 10:49:31 +0100

    feat: edit and delete own comments

--- src/app/features/ticket/find/comments/comments.component.ts
+++ src/app/features/ticket/find/comments/comments.component.ts
@@ -2,7 +2,7 @@ import { Component, Input, Inject, OnDestroy, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { Editor } from 'ngx-editor';
 import { Subject, takeUntil } from 'rxjs';
-import { CommentAddRequest, Comments } from 'src/app/core/models/comment.model';
+import { CommentAddRequest, CommentEditRequest, Comments } from 'src/app/core/models/comment.model';
 import { Tickets } from 'src/app/core/models/ticket.model';
 import { User } from 'src/app/core/models/user.model';
 import { CommentService } from 'src/app/core/services/comment.service';
@@ -18,10 +18,13 @@ export class CommentsComponent implements OnInit, OnDestroy {
   comments: Comments[] = []
   title = ''
   ticket_id: number
+  comment_id!: number
 
   editor!: Editor
   editorContent = ''
   showEditor = false
+  editMode = false
+  submitEditedComment = false
 
   currentUser!: User
   destroy$ = new Subject<void>()
@@ -74,13 +77,45 @@ export class CommentsComponent implements OnInit, OnDestroy {
   submit() {
     const comment: CommentAddRequest = {
       content: this.editorContent,
-      userEmail: this.currentUser.email,
-      ticket_id: this.data.ticket.id
+      userEmail: this.currentUser?.email,
+      ticket_id: this.data.ticket?.id
     }
 
     this.commentService.submitComment(comment).subscribe(
-      res => {
+      () => {
         this.commentService.commentSubject.next()
+        this.editorContent = ''
+        this.showEditor = false
+      }
+    )
+  }
+
+  editComment(comment: Comments) {
+    this.editMode = true
+    this.editorContent = comment.content
+    this.comment_id = comment.id
+    this.showEditor = false
+  }
+
+  editCommentCall() {
+    let request: CommentEditRequest = {
+      comment_id: this.comment_id,
+      content: this.editorContent,
+      user_email: this.currentUser?.email
+    }
+    this.editMode = false
+
+    this.commentService.editComment(request).subscribe(
+      () => {
+        this.reloadComments()
+      }
+    )
+  }
+
+  deleteComment(id: number) {
+    this.commentService.deleteComment(id).subscribe(
+      () => {
+        this.reloadComments()
       }
     )
   }
